b9f944d30be1551285016d65d30c9a1df3907d9b
jaeger <chjie.jaeger@gmail.com>
2018-06-03 21:44:42 +0800

    remove consolde code

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,4 @@
 chrome.tabs.onCreated.addListener(function (tab) {
-  console.log(tab)
-  console.log(tab.url)
-  console.log('fffffffffffff')
   if (checkIsDuckLink(tab.url)) {
     let originalUrl = getOriginalUrl(tab.url)
     chrome.tabs.update(tab.id, {
@@ -22,6 +19,5 @@ function getOriginalUrl (url) {
   end = end === -1 ? url.length : end
   let realUrl = url.substring(start + 5, end)
   realUrl = decodeURIComponent(realUrl)
-  console.log(realUrl)
   return realUrl
 }
